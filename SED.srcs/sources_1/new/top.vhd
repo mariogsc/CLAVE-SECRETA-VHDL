2a1588e45a6761cc1a0517d26a02b701c2db0984
MARIO GÓMEZ SÁNCHEZ-CELEMÍN <mario.gomez.sanchez-celemin@alumnos.upm.es>
2023-12-03 12:15:30 +0100

    se añade multiplexación en el timpo
    - añadir un led para cada estado
    - probar en fpga
    - añadir genericos
    - cambiar y reorganizar nombres

--- SED.srcs/sources_1/new/top.vhd
+++ SED.srcs/sources_1/new/top.vhd
@@ -6,20 +6,19 @@ USE ieee.std_logic_unsigned.ALL;
 ENTITY top IS
  PORT ( 
    num : IN std_logic_vector(3 DOWNTO 0);
-   --digsel : IN std_logic_vector(3 DOWNTO 0);
    boton: IN std_logic;
    reset: IN std_logic;
    CLK_top: IN std_logic;
-   led_p: out std_logic;
+   led_p: out std_logic; -- AÑADIR UN LED PARA CADA ESTADO (MIRAR FPGA PARA VER EL ORDEN)
    
    contrasena: OUT std_logic_vector(3 DOWNTO 0);
-   --digctrl : OUT std_logic_vector(3 DOWNTO 0);
    segment : OUT std_logic_vector(6 DOWNTO 0)
  );
 END top;
 
 ARCHITECTURE BEHAVIORAL of top is
-type STATES is (s0,s1,s2,s3,s4);
+type STATES is (s0,s1,s2,s3,s4,s5);
+type segments_value is array(3 downto 0) of std_logic_vector(6 downto 0);
   COMPONENT decoder
     PORT (
     entrada : IN std_logic_vector(3 DOWNTO 0);
@@ -40,12 +39,17 @@ type STATES is (s0,s1,s2,s3,s4);
   signal current_state: STATES:= s0;
   signal next_state: STATES;
   signal Internal_CLK, syncin_edge, boton_sig: std_logic;
+  signal segment_sig: std_logic_vector(6 downto 0);
+  signal value: segments_value; -- Para guardar el valor de cada 7 segmentos
+  signal aux: positive:=1; -- Para hacer multiplexación en el tiempo de 7 segmentos
 BEGIN
-
+  
   Inst_decoder: decoder PORT MAP (
   entrada =>num ,
-  led =>segment
+  led =>segment_sig -- Se ha cambiado la salida segment por segment_sig (signal)
   );
+  
+  segment<=segment_sig; -- Se asigna el valor de la señal al display 7 segs ( se puede hacer mediante otra instancición mirar)
   
   inst_edge: EDGEDCTR
     Port map (
@@ -81,16 +85,24 @@ BEGIN
      when s1 =>
      if boton_sig= '1' then
      next_state <= s2;
+     value(0)<=segment_sig;
      end if;
      when s2 =>
      if boton_sig = '1' then
      next_state <= s3;
+     value(1)<=segment_sig;
      end if;
      when s3 =>
      if boton_sig = '1' then
      next_state <= s4;
+     value(2)<=segment_sig;
      end if;
      when s4 =>
+     if boton_sig = '1' then
+     next_state <= s0;
+     value(3)<=segment_sig;
+     end if;
+     when s5 =>
      if boton_sig = '1' then
      next_state <= s0;
      end if;
@@ -99,7 +111,7 @@ BEGIN
      end case;
     end process;
     
-     output_decod: process (current_state)
+     output_decod: process (current_state,CLK_top)
      begin
      case current_state is
      when S0 =>
@@ -113,6 +125,25 @@ BEGIN
      contrasena <= "1011";
      when S4 =>
      contrasena <= "0111";
+     when S5 =>
+     -- Multiplexación en el tiempo de cada display (más veloz que ojo humano)
+        if rising_edge(CLK_top) then
+            if(aux=1) then
+                contrasena <= "1110";
+                segment_sig<=value(0);
+            elsif(aux=2) then
+                 contrasena <= "1101";
+                 segment_sig<=value(1);
+            elsif(aux=3) then
+                 contrasena <= "1011";
+                 segment_sig<=value(2);
+            elsif(aux=4) then
+                 contrasena <= "0111";
+                 segment_sig<=value(3);
+                aux<=0;
+            end if;
+            aux<=aux+1;
+        end if;
      when others =>
      contrasena <= (OTHERS => '1');
      end case;
